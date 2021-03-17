2e892b8ffc9d885a689b0f7d58942f5514e7dc5a
Adrix00 <adrianreyescano@gmail.com>
2021-03-17 09:42:06 +0100

    codigo restaurado

--- torneos_Adrian_Mikel/src/main/java/com/antartyca/torneos_Adrian_Mikel/repositories/EquipoRepository.java
+++ torneos_Adrian_Mikel/src/main/java/com/antartyca/torneos_Adrian_Mikel/repositories/EquipoRepository.java
@@ -1,15 +1,12 @@
 package com.antartyca.torneos_Adrian_Mikel.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.antartyca.torneos_Adrian_Mikel.models.EquipoModel;
 
-<<<<<<< HEAD
 
-=======
 @Repository
->>>>>>> 594de72 (errores de repositorios corregidos)
 public interface EquipoRepository extends JpaRepository<EquipoModel, Integer> {
 
 	
--- torneos_Adrian_Mikel/src/main/java/com/antartyca/torneos_Adrian_Mikel/repositories/TorneoRepository.java
+++ torneos_Adrian_Mikel/src/main/java/com/antartyca/torneos_Adrian_Mikel/repositories/TorneoRepository.java
@@ -1,10 +1,6 @@
 package com.antartyca.torneos_Adrian_Mikel.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.data.repository.CrudRepository;
-=======
->>>>>>> 594de72 (errores de repositorios corregidos)
 import org.springframework.stereotype.Repository;
 
 import com.antartyca.torneos_Adrian_Mikel.models.TorneoModel;
